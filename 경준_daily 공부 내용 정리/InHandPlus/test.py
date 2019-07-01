73f968982a05897b1815b4d1946774e8b3032167
kyungjunlee91 <kyungjunlee.me@gmail.com>
2019-07-01 17:29:15 +0900

    1일 1 commit  2019년 하반기 시작

---
+++ 경준_daily 공부 내용 정리/InHandPlus/test.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw
+import sys
+
+im = Image.open("D:\\0619_data_완료\\tl_0001_0001_20190619_051738.jpg")
+
+draw = ImageDraw.Draw(im)
+draw.line((0, 0) + im.size, fill=128)
+draw.line((0, im.size[1], im.size[0], 0), fill=128)
+del draw
+
+# write to stdout
+im.save(sys.stdout, "PNG")
